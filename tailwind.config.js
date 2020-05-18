6a618329f26b595e70eb8e0e229244e66a140624
vlad <vlad.blindu@gmail.com>
2020-05-18 11:01:24 +0300

    tailwind.config.fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 const theme = require('./src/assets/theme')
 
+const colors = {
+    'ui-primary': '#283344',
+    'ui-light': '#f6e05e',
+    "ui-hover": '#344a6e',
+}
+
 module.exports = {
     theme: {
         extend:{
-            colors:{
-                'ui-primary': '#283344',
-                'ui-light': '#f6e05e',
-                "ui-hover": '#344a6e',
-            },
-            ...theme
+            ...colors,
+            ...theme['colors']
         }
     },
     variants: {},
