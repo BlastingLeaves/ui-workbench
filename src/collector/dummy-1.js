e53b4d7cff05f709e0f2b9798701d6104af78c78
vlad <vlad.blindu@gmail.com>
2020-05-19 21:53:33 +0300

    added suppoert for async/await

--- src/collector/dummy-1.js
+++ src/collector/dummy-1.js
@@ -1,4 +1,8 @@
 import React from 'react'
 import Dummy1 from '../components/dummy-1'
+import testImg from '../assets/img/test-img.png'
 
-export default () => <Dummy1 />
+export default () => <div>
+    <Dummy1 />
+    <img src={testImg} alt="if you see this it's wrong" />
+</div>
