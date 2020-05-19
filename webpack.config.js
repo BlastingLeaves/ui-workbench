b6fa1f57b49fd187369880d8524220316185902c
vlad <vlad.blindu@gmail.com>
2020-05-19 18:58:35 +0300

    added to webpack:  - file-loader (png/gij/jpg/jpeg)  - json-loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,18 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
             }
         ]
     },
